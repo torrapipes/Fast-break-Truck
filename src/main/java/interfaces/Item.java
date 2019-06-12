c2e9c13710d1ff7a38a66a4ceaa0e4dc65452ae5
torrapipes <crbergas@gmail.com>
2019-06-12 18:04:23 +0200

    interface añadidas

---
+++ src/main/java/interfaces/Item.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+public interface Item {
+
+    String nombre();
+
+    Packing empaquetado();
+
+    Float pvp();
+
+}
---
+++ src/main/java/interfaces/Packing.java
@@ -0,0 +1,7 @@
+package interfaces;
+
+public interface Packing {
+
+    String envoltorio();
+
+}
